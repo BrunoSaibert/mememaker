f898e31d909c752966c63d2ed2952b1fe793ed02
BrunoSaibert <brunosaibert@gmail.com>
2020-06-14 00:17:57 -0300

    :art: Adicionar estilos globais

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import GlobalStyles from "./styles/global";
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <h1>App</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
